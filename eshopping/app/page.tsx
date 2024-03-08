10c607aa8d52fdcb7966a1e991e04a51002fd439
Hans Arthur Cupiterson <125373236+KetsunoAna117@users.noreply.github.com>
2024-03-08 23:26:06 +0700

    Finishing Navbar

--- eshopping/app/page.tsx
+++ eshopping/app/page.tsx
@@ -1,21 +1,25 @@
+"use client";
+
 import eshoppingLogo from "./assets/eshoppingLogo.png";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import OptionMenu from "./components/option-menu";
 
 export default function Home() {
   return (
-    <div className="flex justify-center w-screen">
+    <div className="flex items-center justify-center w-full flex-col">
       {/* Navbar */}
       <div className="w-11/12 mt-6 flex items-center justify-between">
         {/* Leftside */}
         <div className="flex items-center">
           <Image src={eshoppingLogo} alt="eShopping" className="w-20 h-20" />
           <div className="ms-5 ">
-            <h4 className="text-lg font-bold">EShopping</h4>
+            <h4 className="text-lg font-bold">E-Shopping</h4>
             <h6 className="font-extralight text-xs">Everything is on sale!</h6>
           </div>
         </div>
         {/* RightSide */}
-        <div></div>
+        <OptionMenu />
       </div>
     </div>
   );
