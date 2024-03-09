7554ced37925953072e6d63d2226b3e13c2eab1c
Hans Arthur Cupiterson <125373236+KetsunoAna117@users.noreply.github.com>
2024-03-09 16:33:19 +0700

    Update option-menu.tsx

--- eshopping/components/option-menu.tsx
+++ eshopping/components/option-menu.tsx
@@ -1,10 +1,27 @@
+import { Icon } from "@iconify-icon/react";
+
 export default function OptionMenu() {
   return (
-    <div className="z-40 max-md:fixed max-md:bottom-0 max-md:left-0 max-md:w-full flex items-center justify-center max-md:py-6 max-md:bg-zinc-900">
-      <ul className="option-menu flex cursor-pointer text-base font-light justify-around w-6/12 *:md:mr-4 select-none">
-        <li className="hover:underline">Home</li>
-        <li className="hover:underline">Cart</li>
-        <li className="hover:underline">MyShop</li>
+    <div className="z-40 max-md:fixed max-md:bottom-0 max-md:left-0 max-md:w-full flex items-center justify-center max-md:py-4 max-md:bg-zinc-900">
+      <ul className="option-menu flex cursor-pointer text-base font-light justify-around w-full max-md:w-8/12 md:*:mr-3 *:flex *:items-center *:justify-center *:max-md:flex-col">
+        <li className="hover:underline">
+          <Icon
+            icon="material-symbols:home"
+            className="md:mr-1 text-lg max-md:text-3xl"
+          />
+          <p className="max-md:text-xs">Home</p>
+        </li>
+        <li className="hover:underline">
+          <Icon icon="mdi:cart" className="md:mr-1 text-lg max-md:text-3xl" />
+          <p className="max-md:text-xs">Cart</p>
+        </li>
+        <li className="hover:underline">
+          <Icon
+            icon="lets-icons:shop"
+            className="md:mr-1 text-lg max-md:text-3xl"
+          />
+          <p className="max-md:text-xs">My Shop</p>
+        </li>
       </ul>
     </div>
   );
